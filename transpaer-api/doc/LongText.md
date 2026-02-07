4315fd7b325f4301772b950a30beb658b688f231
proton.decay <2070114+proton-decay@users.noreply.github.com>
2026-02-07 17:47:26 +0100

    Update api and schemas (#12)

--- transpaer-api/doc/LongText.md
+++ transpaer-api/doc/LongText.md
@@ -9,7 +9,7 @@ import 'package:transpaer_api/api.dart';
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **text** | **String** | Long string for descriptions, articles... | 
-**source_** | **String** | Describes where the related data was retrieved from. | 
+**sources** | **List<String>** | Describes where the related data was retrieved from. | [default to const []]
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- transpaer-api/lib/model/long_text.dart
+++ transpaer-api/lib/model/long_text.dart
@@ -14,33 +14,33 @@ class LongText {
   /// Returns a new [LongText] instance.
   LongText({
     required this.text,
-    required this.source_,
+    this.sources = const [],
   });
 
   /// Long string for descriptions, articles...
   String text;
 
   /// Describes where the related data was retrieved from.
-  String source_;
+  List<String> sources;
 
   @override
   bool operator ==(Object other) => identical(this, other) || other is LongText &&
     other.text == text &&
-    other.source_ == source_;
+    _deepEquality.equals(other.sources, sources);
 
   @override
   int get hashCode =>
     // ignore: unnecessary_parenthesis
     (text.hashCode) +
-    (source_.hashCode);
+    (sources.hashCode);
 
   @override
-  String toString() => 'LongText[text=$text, source_=$source_]';
+  String toString() => 'LongText[text=$text, sources=$sources]';
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
       json[r'text'] = this.text;
-      json[r'source'] = this.source_;
+      json[r'sources'] = this.sources;
     return json;
   }
 
@@ -64,7 +64,9 @@ class LongText {
 
       return LongText(
         text: mapValueOfType<String>(json, r'text')!,
-        source_: mapValueOfType<String>(json, r'source')!,
+        sources: json[r'sources'] is Iterable
+            ? (json[r'sources'] as Iterable).cast<String>().toList(growable: false)
+            : const [],
       );
     }
     return null;
@@ -113,7 +115,7 @@ class LongText {
   /// The list of required keys that must be present in a JSON.
   static const requiredKeys = <String>{
     'text',
-    'source',
+    'sources',
   };
 }
 
